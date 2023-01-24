ab4d68cef932d5384e51a12b89d9c99f1d6a2824
RomanBratus <romanbratus@gmail.com>
2023-01-24 19:22:16 +0200

    update task 1

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+const renderGalleryItems = (pictures) =>
+    pictures.reduce(
+        (acc, picture) =>
+            acc +
+            `<a class="gallery__item" href=${picture.original}>
+      <img class="gallery__image" src=${picture.preview} alt=${picture.description} />
+    </a>
+    `,
+        ""
+    );
 
-console.log(galleryItems);
+const insertGalleryItems = (string) => {
+
+    gallery.insertAdjacentHTML("beforeend", string);
+};
+insertGalleryItems(renderGalleryItems(galleryItems));
+
+const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+    scrollZoom: false,
+});
