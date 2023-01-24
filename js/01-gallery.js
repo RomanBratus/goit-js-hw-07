dd5a4aee35512f6bfff12f588df39b20068d02be
RomanBratus <romanbratus@gmail.com>
2023-01-24 19:14:20 +0200

    update task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,53 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const renderGalleryItems = (pictures) => {
+    return pictures.reduce(
+        (acc, picture) =>
+            acc +
+            `<div class="gallery__item">
+  <a class="gallery__link" href=${picture.original}>
+    <img
+      class="gallery__image"
+      src=${picture.preview}
+      data-source=${picture.original}
+      alt=${picture.description}
+    />
+  </a>
+</div>`,
+        ""
+    );
+};
+const insertGalleryItems = (string) => {
+
+    gallery.insertAdjacentHTML("beforeend", string);
+};
+insertGalleryItems(renderGalleryItems(galleryItems));
+
+gallery.addEventListener("click", onGalleryClick);
+
+function onGalleryClick(evt) {
+    evt.preventDefault();
+
+    if (!evt.target.classList.contains("gallery__image")) {
+        return;
+    }
+    const instance = basicLightbox.create(
+        `<img src=${evt.target.dataset.source} width="800" height="600" />`,
+        {
+            onShow: instance => {
+                window.addEventListener('keydown', closeModal);
+            },
+            onClose: instance => {
+                window.removeEventListener('keydown', closeModal);
+            },
+        }
+    );
+    function closeModal(evt) {
+        if (evt.code === 'Escape') {
+            instance.close();
+        }
+    }
+    instance.show();
+}
